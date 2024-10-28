84bc5e7365bb9ff4ca9ec8b087c9f258785bf25a
HKThanh <21086351.thanh@student.iuh.edu.vn>
2024-10-28 20:07:06 +0700

    ReactNative apply redux toolkit for increase and decrease number by clicking button

--- src/Tuan09/App.js
+++ src/Tuan09/App.js
@@ -1,27 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
+import { store } from "./store";
+import { Provider } from "react-redux";
 
 import Reducer from './Screens/useReducer';
+import Redux from './Screens/Redux';
 
 const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Drawer.Navigator>
-        <Drawer.Screen name="Reducer" component={Reducer} />
-      </Drawer.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Drawer.Navigator>
+          <Drawer.Screen name="Reducer" component={Reducer} />
+          <Drawer.Screen name="Redux" component={Redux} />
+        </Drawer.Navigator>
+      </NavigationContainer>
+    </Provider>
+    
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/Tuan09/Screens/Redux.js
@@ -0,0 +1,19 @@
+import { View, Text, SafeAreaView, Button } from "react-native";
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement, selectCount } from "../redux/counterSlice";
+
+const Redux = () => {
+    const count = useSelector(selectCount);
+    const dispatch = useDispatch();
+
+    return (
+        <SafeAreaView style={{flex: 1, alignItems: 'center', justifyContent: 'space-evenly'}}>
+            <Text style={{fontSize: 40, fontWeight: '700'}}>Count: {count}</Text>
+            <Button title="Increment" onPress={() => dispatch(increment())} />
+            <Button title="Decrement" onPress={() => dispatch(decrement())} />
+        </SafeAreaView>
+    )
+}
+
+export default Redux;
